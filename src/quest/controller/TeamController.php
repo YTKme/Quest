e8f217c1df08c9882a8d74111ab60c764a867ae3
Yan Kuang <YTKColumba@Yahoo.com>
2014-01-06 15:18:01 -0800

    Added TeamController with new functions for TeamModel

---
+++ src/quest/controller/TeamController.php
@@ -0,0 +1,243 @@
+<?php
+
+use Silex\Application;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\DBAL\DBALException;
+
+class TeamController implements ControllerInterface {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct () {
+
+	}
+	
+	/**
+	 * Add team
+	 *
+	 * @method POST
+	 * @param Request $request
+	 * @param Application $application
+	 * @return Response
+	 */
+	public function add (Request $request, Application $application) {
+		// JSON and POST
+		if (strpos($request->headers->get('Content-Type'), 'application/json') === 0 && strpos($request->getMethod(), ControllerInterface::HTTP_METHOD_POST) === 0) {
+			// Get JSON data
+			if (!$jsonData = json_decode($request->getContent(), true)) {
+				return new Response('ERROR: Bad request.', 400);
+			}
+				
+			// Parse JSON data
+			$request->request->replace(is_array($jsonData) ? $jsonData : array());
+			
+			try {
+				// Create an array to store the team created and read
+				$teamArray = array();
+					
+				foreach ($jsonData as $team) {
+					// Check if the team exist
+					if (!$teamModel = $application['quest.orm.manager']->getRepository('TeamModel')->findOneBy(array('name' => $team['name']))) {
+						// Create team
+						$teamModel = new TeamModel(
+							NULL, // ID
+							empty($team['name'])
+								? NULL
+								: $team['name'],
+							empty($team['point'])
+								? 0
+								: $team['point']
+						);
+							
+						// Store team
+						$application['quest.orm.manager']->persist($teamModel);
+					}
+			
+					// Push created and or read team into the array
+					array_push($teamArray, $teamModel->toArray());
+				}
+					
+				// Synchronize with database
+				$application['quest.orm.manager']->flush();
+			} catch (DBALException $exception) {
+				return
+					$application['debug']
+						? new Response('DBAL Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Unable to add team.', 500);
+			} catch (Exception $exception) {
+				return
+					$application['debug']
+						? new Response('Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Failure.', 500);
+			}
+				
+			return $application->json($teamArray, 201);
+			
+		}
+		
+		return new Response('ERROR: Bad request.', 400);
+	}
+	
+	/**
+	 * Retrieve team
+	 *
+	 * @method GET
+	 * @param Request $request
+	 * @param Application $application
+	 * @return Response
+	*/
+	public function retrieve (Request $request, Application $application) {
+		// JSON and GET
+		if (strpos($request->headers->get('Content-Type'), 'application/json') === 0 && strpos($request->getMethod(), ControllerInterface::HTTP_METHOD_GET) === 0) {
+			try {
+				// Check if the team exist
+				if ($teamModels = $application['quest.orm.manager']->getRepository('TeamModel')->findAll()) {
+					// Convert team object to array
+					foreach ($teamModels as $key => $value) {
+						$teamModels[$key] = $teamModels[$key]->toArray();
+					}
+		
+					return $application->json($teamModels, 200);
+				}
+			} catch (DBALException $exception) {
+				return
+					$application['debug']
+						? new Response('DBAL Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Unable to retrieve team.', 500);
+			} catch (Exception $exception) {
+				return
+					$application['debug']
+						? new Response('Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Failure.', 500);
+			}
+		
+			return new Response('ERROR: Unable to retrieve team.', 404);
+		}
+		
+		return new Response('ERROR: Bad request.', 400);
+	}
+	
+	/**
+	 * Edit team
+	 *
+	 * @method PUT
+	 * @param Request $request
+	 * @param Application $application
+	 * @return Response
+	*/
+	public function edit (Request $request, Application $application) {
+		// JSON and PUT
+		if (strpos($request->headers->get('Content-Type'), 'application/json') === 0 && strpos($request->getMethod(), ControllerInterface::HTTP_METHOD_PUT) === 0) {
+			// Get JSON data
+			if (!$jsonData = json_decode($request->getContent(), true)) {
+				return new Response('ERROR: Bad request.', 400);
+			}
+		
+			// Parse JSON data
+			$request->request->replace(is_array($jsonData) ? $jsonData : array());
+			
+			try {
+				// Create an array to store the team updated
+				$teamArray = array();
+			
+				foreach ($jsonData as $team) {
+					// Check if the team exist
+					if ($teamModel = $application['quest.orm.manager']->getRepository('TeamModel')->findOneBy(array('id' => $team['id']))) {
+						// Update team
+						$teamModel->setName(
+							empty($team['name'])
+								? $teamModel->getName()
+								: $team['name']
+						);
+						$teamModel->setPoint(
+							empty($team['point'])
+								? $teamModel->getPoint()
+								: $team['point']
+						);
+			
+						// Update team
+						$application['quest.orm.manager']->persist($teamModel);
+			
+						// Push updated team into the array
+						array_push($teamArray, $teamModel->toArray());
+					}
+				}
+			
+				// Synchronize with database
+				$application['quest.orm.manager']->flush();
+			} catch (DBALException $exception) {
+				return
+					$application['debug']
+						? new Response('DBAL Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Unable to edit team.', 500);
+			} catch (Exception $exception) {
+				return
+					$application['debug']
+						? new Response('Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Failure.', 500);
+			}
+			
+			return $application->json($teamArray, 200);
+		}
+		
+		return new Response('ERROR: Bad request.', 400);
+	}
+	
+	/**
+	 * Remove team
+	 *
+	 * @method DELETE
+	 * @param Request $request
+	 * @param Application $application
+	 * @return Response
+	*/
+	public function remove (Request $request, Application $application) {
+		// JSON and DELETE
+		if (strpos($request->headers->get('Content-Type'), 'application/json') === 0 && strpos($request->getMethod(), 'DELETE') === 0) {
+			// Get JSON data
+			if (!$jsonData = json_decode($request->getContent(), true)) {
+				return new Response('ERROR: Bad request.', 400);
+			}
+		
+			// Parse JSON data
+			$request->request->replace(is_array($jsonData) ? $jsonData : array());
+			
+			try {
+				// Create an array to store the team deleted
+				$teamArray = array();
+			
+				foreach ($jsonData as $team) {
+					// Check if the team exist
+					if ($teamModel = $application['quest.orm.manager']->getRepository('TeamModel')->findOneBy(array('id' => $team['id']))) {
+						// Delete team
+						$application['quest.orm.manager']->remove($teamModel);
+			
+						// Push deleted team into the array
+						array_push($teamArray, $teamModel->toArray());
+					}
+				}
+			
+				// Synchronize with database
+				$application['quest.orm.manager']->flush();
+			} catch (DBALException $exception) {
+				return
+					$application['debug']
+						? new Response('DBAL Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Unable to delete team.', 500);
+			} catch (Exception $exception) {
+				return
+					$application['debug']
+						? new Response('Exception: ' . $exception->getMessage(), 500)
+						: new Response('ERROR: Failure.', 500);
+			}
+			
+			return $application->json($teamArray, 200);
+		}
+		
+		return new Response('ERROR: Bad request.', 400);
+	}
+	
+}
--- src/quest/model/TeamModel.php
+++ src/quest/model/TeamModel.php
@@ -1,6 +1,7 @@
 <?php
 
 use Doctrine\ORM\Id\SequenceGenerator;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @Entity
@@ -26,6 +27,12 @@ class TeamModel {
 	protected $point = NULL;
 	
 	/**
+	 * @ManyToMany(targetEntity="EventModel", inversedBy="teams")
+	 * @JoinTable(name="team_event")
+	 */
+	protected $events = NULL;
+	
+	/**
 	 * Constructor
 	 * 
 	 * @param string $id
@@ -40,6 +47,21 @@ class TeamModel {
 		$this->id = $id;
 		$this->name = $name;
 		$this->point = $point;
+		
+		$this->events = new ArrayCollection();
+	}
+	
+	/**
+	 * To array
+	 * 
+	 * @return array
+	 */
+	public function toArray () {
+		return array(
+			'id' => $this->getId(),
+			'name' => $this->getName(),
+			'point' => $this->getPoint()
+		);
 	}
 	
 	/**
@@ -85,6 +107,38 @@ class TeamModel {
 	 */
 	public function setPoint ($point = NULL) {
 		$this->point = $point;
+	}
+	
+	/**
+	 * Get events
+	 * 
+	 * @return array
+	 */
+	public function getEvents () {
+		// Create new event array
+		$eventArray = array();
+		
+		// Loop through each event in the ArrayCollection
+		foreach ($this->events as $event) {
+			// Add each event to new event array
+			array_push($eventArray, $event->toArray());
+		}
+		
+		// Return new event array
+		return $eventArray;
+	}
+	
+	/**
+	 * Add event
+	 * 
+	 * @param EventModel $event
+	 */
+	public function addEvent (EventModel $event = NULL) {
+		// Synchronously updating inverse side
+		$event->addTeam($this);
+		
+		// Add event to the ArrayCollection
+		$this->events[] = $event;
 	}
 	
 }
