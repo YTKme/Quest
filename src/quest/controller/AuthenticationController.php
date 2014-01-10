cdb6fdfcbe360f88ce0cbff7db20d371ce0c73b0
Yan Kuang <YTKColumba@Yahoo.com>
2014-01-10 09:33:45 -0800

    Added logout function

--- src/quest/controller/AuthenticationController.php
+++ src/quest/controller/AuthenticationController.php
@@ -4,8 +4,8 @@ use Silex\Application;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\DBAL\DBALException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\DBAL\DBALException;
 
 class AuthenticationController {
 	
@@ -28,13 +28,36 @@ class AuthenticationController {
 		$username = $request->request->get("txtUsername");
 		$password = $request->request->get("txtPassword");
 		$hashPassword = hash('sha256', hash('sha256', $password) . $username);
-		$host = $application['request']->getSchemeAndHttpHost();
+		$host = $request->getSchemeAndHttpHost();
 		
 		if (!$userModel = $application['quest.orm.manager']->getRepository('UserModel')->findOneBy(array('username' => $username, 'password' => $hashPassword))) {
+			// Redirect back to login
 			return new RedirectResponse($host);
 		}
 		
-		return new Response("Logged In", 200);
+		// Save username to session
+		$application['session']->set('_USERNAME', $username);
+		
+		// Redirect to main
+		return new RedirectResponse($host . '/main');
+	}
+	
+	/**
+	 * Logout
+	 *
+	 * @method GET
+	 * @param Request $request
+	 * @param Application $application
+	 * @return Response
+	 */
+	public function logout (Request $request, Application $application) {
+		$host = $request->getSchemeAndHttpHost();
+		
+		// Clear session
+		$application['session']->set('_USERNAME', "");
+		$application['session']->clear();
+		
+		return new RedirectResponse($host);
 	}
 	
 }
