f5402627697cd4b85838e5ad574aac717ce8af5f
Scott Sowers <scott.sowers@live.com>
2014-03-28 23:22:56 -0700

    Headers

--- src/quest/controller/TeamController.php
+++ src/quest/controller/TeamController.php
@@ -129,7 +129,9 @@ class TeamController implements ControllerInterface {
 			}
 				
 			return $application->json($teamArray, 201, array(
-                        'Access-Control-Allow-Origin' => '*'
+                        'Access-Control-Allow-Origin' => '*',
+                        'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+                        'Access-Control-Allow-Headers' => 'Content-Type'
                     ));
 			
 		}
