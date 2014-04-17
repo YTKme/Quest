817ef80edc1326f7b69866ec122c92e80e4419be
yakuang <yakuang@cisco.com>
2014-04-17 10:35:41 -0700

    Update index with CORS test

--- web/index.php
+++ web/index.php
@@ -2,3 +2,8 @@
 
 $application = require __DIR__ . '/../src/quest/quest.php';
 $application->run();
+
+// CORS
+$application->after(function (Request $request, Response $response) {
+	$response->headers->set('Access-Control-Allow-Origin', '*');
+});
